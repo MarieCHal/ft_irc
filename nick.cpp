8f6660db7079dfd61a3b6c497cc22a679e936a40
jschreye <jschreye@c1r4s2.42lausanne.ch>
2022-12-21 14:34:05 +0100

    nick

--- nick.cpp
+++ nick.cpp
@@ -2,22 +2,18 @@
 // 9 caratere maximum
 int cmd_nick(t_data *data, int i, std::string cmd)
 {
-    int k = 0;
-    for (size_t j = 0; j < cmd.length(); j++)
-    {
-        if (cmd[j] == ' ')
-            k++;
-    }
-    if (k != 1)
-        return (1);
     size_t pos = cmd.find_first_of(' ', 0);
     cmd.erase(0, pos + 1);
-    size_t j = 0;
-    while (cmd.compare(data->client[j].nickname) != 0 && j < data->client.size())
+    if (cmd.find(' '))
+        return(1);
+    int j = 0;
+    while (j < data->max_client)
+    {
+        if (cmd.compare(data->client[j].nickname) == 0)
+            return (1);
         j++;
-    if (cmd.compare(data->client[j].nickname) == 0)
-        return (1);
-    else if (cmd.size() > 9)
+    }
+    if (cmd.size() > 9)
         return (1);
     data->client[i].nickname = cmd;
     return (0);
